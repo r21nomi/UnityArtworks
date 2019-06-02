62c781b5ebb62dbe09f5c0771f366f4aba2a23aa
r21nomi <r21nomi@gmail.com>
2019-06-02 12:45:01 +0900

    Add 04_3_vfx_graph

---
+++ 04_3_vfx_graph/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float x;
+
+    // Update is called once per frame
+    void Update()
+    {
+        x += Time.deltaTime * 0.05f;
+        this.transform.Rotate(0.0f, x, 0.0f);
+    }
+}
---
+++ 04_3_vfx_graph/README.md
@@ -0,0 +1,7 @@
+# 04_3_vfx_graph
+## How to use VFX Graph
+- referred to https://qiita.com/tan-y/items/cd6fc58674d6f0c54d0b
+
+## How to record game view
+- Unity Recorder was used
+- referred to https://qiita.com/tan-y/items/644760a18484cbe71d43

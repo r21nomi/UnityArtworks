8ee4729416f629e4a59340cf71aebc4717cb339f
r21nomi <r21nomi@gmail.com>
2019-06-03 12:26:51 +0900

    Add 04_4_vfx_graph

---
+++ 04_4_vfx_graph/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float r;
+
+    // Update is called once per frame
+    void Update()
+    {
+        r = Time.deltaTime * 20.5f;
+        this.transform.Rotate(r, r, r);
+    }
+}
---
+++ 04_4_vfx_graph/README.md
@@ -0,0 +1,4 @@
+# 04_4_vfx_graph
+![](art/art.PNG)
+## How to use VFX Graph
+- referred to https://notargs.hateblo.jp/entry/vfx_graph_tutorial

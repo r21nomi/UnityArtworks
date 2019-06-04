7a4327bf42624fa0bb47e598787ad390d7d536d1
r21nomi <r21nomi@gmail.com>
2019-06-04 10:01:55 +0900

    Add 04_5_vfx_graph

---
+++ 04_5_vfx_graph/Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject  target;
+    public float rotateSpeed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float r = 0;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        r += Time.deltaTime * rotateSpeed;
+
+        this.transform.RotateAround
+        ( 
+            target.transform.position, 
+            Vector3.up, 
+            r
+        );
+    }
+}
---
+++ 04_5_vfx_graph/README.md
@@ -0,0 +1,6 @@
+# 04_5_vfx_graph
+
+![](art/art.PNG)
+
+## Resource
+- https://www.youtube.com/watch?time_continue=6&v=R6D1b7zZHHA

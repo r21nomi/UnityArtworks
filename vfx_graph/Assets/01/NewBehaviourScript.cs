d55c0f6a73d103b0ae573ad075b49bbd7c102e53
r21nomi <r21nomi@gmail.com>
2019-06-08 19:47:31 +0900

    Add vfx_graph 01

---
+++ vfx_graph/Assets/01/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public GameObject target;
+    private float r = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        r = Time.deltaTime * 40;
+        
+        this.transform.RotateAround
+        ( 
+            target.transform.position, 
+            Vector3.up, 
+            r
+        );
+    }
+}

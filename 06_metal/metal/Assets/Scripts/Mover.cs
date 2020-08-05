e0a80bc94a07db44607b24a0f141e53959b11416
r21nomi <r21nomi@gmail.com>
2020-08-05 23:14:36 +0900

    Add metal

---
+++ 06_metal/metal/Assets/Scripts/Mover.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float offset = 0;
+    private float initialY = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        pos.y = initialY + initialY * (float) Math.Sin(Time.time + offset) * 0.5f;
+        transform.position = pos;
+    }
+}
---
+++ 06_metal/metal/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private List<Mover> movers = new List<Mover>();
+    private int moverCount = 18;
+    public GameObject moverPref;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < moverCount; i++)
+        {
+            GameObject instancedMoverPref = Instantiate(moverPref, new Vector3(i, 1.0f, 0), moverPref.gameObject.transform.rotation);
+            Mover mover = instancedMoverPref.GetComponent<Mover>();
+            mover.offset = i * 0.4f;
+            movers.Add(mover);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
